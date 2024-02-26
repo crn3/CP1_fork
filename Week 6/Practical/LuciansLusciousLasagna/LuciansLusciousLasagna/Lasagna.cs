ab0b774869da1da84d1362624d2c36e8196b398e
Rachael Kenny <157695390+crn3@users.noreply.github.com>
2024-02-26 12:36:17 +0000

    Completed week 6 practical

--- Week 6/Practical/LuciansLusciousLasagna/LuciansLusciousLasagna/Lasagna.cs
+++ Week 6/Practical/LuciansLusciousLasagna/LuciansLusciousLasagna/Lasagna.cs
@@ -19,8 +19,8 @@ namespace LuciansLusciousLasagna
         public int ExpectedMinutesInOven()
         {
             // TODO: Implement the 'ExpectedMinutesInOven()' method
-            throw new NotImplementedException("Please implement the ExpectedMinutesInOven() method");
-
+            //throw new NotImplementedException("Please implement the ExpectedMinutesInOven() method");
+            return 40;
         }
         /// <summary>
         /// The RemainingMinutesInOven method that takes the actual minutes the lasagna has been in
@@ -34,8 +34,8 @@ namespace LuciansLusciousLasagna
         public int RemainingMinutesInOven(int durationInOven)
         {
             // TODO: Implement the 'RemainingMinutesInOven()' method
-            throw new NotImplementedException("Please implement the RemainingMinutesInOven() method");
-
+            //throw new NotImplementedException("Please implement the RemainingMinutesInOven() method");
+            return ExpectedMinutesInOven() - durationInOven;
 
         }
 
@@ -49,7 +49,8 @@ namespace LuciansLusciousLasagna
         public int PreparationTimeInMinutes(int layers)
         {
             // TODO: implement the 'PreparationTimeInMinutes()' method
-            throw new NotImplementedException("Please implement the PreparationTimeInMinutes() method");
+            //throw new NotImplementedException("Please implement the PreparationTimeInMinutes() method");
+            return 2 * layers;
 
         }
 
@@ -67,7 +68,8 @@ namespace LuciansLusciousLasagna
         public int ElapsedTimeInMinutes(int layers, int minutesInOven)
         {
             // TODO: implement the 'ElapsedTimeInMinutes()' method
-            throw new NotImplementedException("Please implement the ElapsedTimeInMinutes() method");
+            //throw new NotImplementedException("Please implement the ElapsedTimeInMinutes() method");
+            return layers*2 + minutesInOven;
         }
     }
 }
